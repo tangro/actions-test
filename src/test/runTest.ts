6a642653ed02538f1e9ef594ead50c09b564825f
ak <ak@tangro.de>
2019-09-04 13:35:12 +0200

    ignore returncode

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,11 +2,13 @@ import * as core from '@actions/core';
 import { exec } from '@actions/exec';
 import * as fs from 'fs';
 import path from 'path';
+import { ExecOptions } from '@actions/exec/lib/interfaces';
 
 export async function runTest() {
   let output = '';
   let errorMessages = '';
-  const options = {
+  const options: ExecOptions = {
+    ignoreReturnCode: true,
     listeners: {
       stdout: (data: Buffer) => {
         output += data.toString();
@@ -19,7 +21,7 @@ export async function runTest() {
   const command = core.getInput('command') || 'test:ci';
 
   try {
-    await exec('npm', ['run', command, '--silent', '2> /dev/null'], options);
+    await exec('npm', ['run', command], options);
     console.log('...', errorMessages);
   } catch (error) {
     console.log('!!!', error);
