40b0579d0b4ddc5450d2def450f5348d19163202
Andy Krings-Stern <ak@tangro.de>
2020-07-07 15:48:59 +0200

    updated dependencies

--- src/test/comment.ts
+++ src/test/comment.ts
@@ -1,5 +1,5 @@
-import { Result } from '../Result';
 import { TestResult } from './parseTests';
+import { Result } from '@tangro/tangro-github-toolkit';
 
 export function createCommentText(results: Result<TestResult>) {
   return `\n## test summary
